08b26aa9be0eba90b450c47aca4b596893b4a0a5
sahilkapoorpnf <sahilkapoorpnf@gmail.com>
2018-08-14 13:26:14 +0530

    Update migration and seed file

---
+++ database/seeds/admin.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class admin extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'type' => 'admin',
+            'password' => bcrypt('admin')
+        ]);
+    }
+}
